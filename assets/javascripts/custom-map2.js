cf3af34219bbec0c41d799e006f4231775c4833c
benjamin lew <bennru33@gmail.com>
2016-02-09 12:57:37 -0800

    updating maps

--- assets/javascripts/custom-map2.js
+++ assets/javascripts/custom-map2.js
@@ -3,7 +3,7 @@ google.maps.event.addDomListener(window, 'load', init);
 
 function init() {
     // The latitude and longitude to center the map (always required)
-    var myLatlng = new google.maps.LatLng(37.785484, -122.269580);
+    var myLatlng = new google.maps.LatLng(37.809104, -122.251358);
     
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
@@ -25,7 +25,7 @@ function init() {
 
     // Get the HTML DOM element that will contain your map 
     // We are using a div with id="map" seen below in the <body>
-    var mapElement = document.getElementById('map-canvas');
+    var mapElement = document.getElementById('map-canvas2');
 
     // Create the Google Map using out element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
